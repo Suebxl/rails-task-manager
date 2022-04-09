62f6f19a279e54a9e33d347ef9956aed38d1bdf7
Suzanne Tordjman <suzannetor@gmail.com>
2022-04-09 12:37:41 -0400

    Add task done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,26 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
